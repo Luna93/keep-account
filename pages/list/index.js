535df19c2819b0554cf94b5ccbd5785e53854e01
liuxingyue <liuxingyue@peilian.com>
2018-04-04 17:43:38 +0800

    list

--- pages/list/index.js
+++ pages/list/index.js
@@ -4,56 +4,27 @@ Page({
     zhichu: "0.00",
     shouru: "0.00",
     currentdate: "2018-04",
-    list: [
-      {
-        id: '30',
-        name: '30号',
-        open: true,
-        pages: ['1','2']
-      }, {
-        id: '29',
-        name: '29号',
-        open: false,
-        pages: ['1', '2', '2', '2', '2']
-      }, {
-        id: '28',
-        name: '28号',
-        open: false,
-        pages: ['1', '2', '2', '2', '2']
-      }, {
-        id: '27',
-        name: '27号',
-        open: false,
-        pages: ['1', '2', '2', '2', '2']
-      }, {
-        id: '26',
-        name: '26号',
-        open: false,
-        pages: ['1', '2', '2', '2']
-      }, {
-        id: '25',
-        name: '25号',
-        pages: ['1', '2', '2', '2', '2']
-      }, {
-        id: '24',
-        name: '24号',
-        pages: ['1', '2', '2']
-      }, {
-        id: '23',
-        name: '23号',
-        pages: ['1', '2', '2', '2', '2']
-      }, {
-        id: '22',
-        name: '22号',
-        pages: ['1', '2', '2', '2', '2']
-      }, {
-        id: '21',
-        name: '21号',
-        pages: ['1', '2', '2', '2', '2']
-      }
-    ]
+    hideHeader: true,
+    hideBottom: true,
+    list: [],
+    refreshTime: '', // 刷新的时间 
+    allPages: '',    // 总页数
+    currentPage: 1,  // 当前页数  默认是1
+    loadMoreData: '加载更多……'
+
   },
-  lower: function(e) {
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var date = new Date();
+    this.setData({
+      refreshTime: date.toLocaleTimeString()
+    })
+    this.getData();
+  },
+  loadMore: function(e) {
     console.log(e);
     console.log('到底了要加载');
   },
@@ -76,6 +47,80 @@ Page({
     this.setData({
       list: list
     });
+  },
+  // 上拉加载更多
+  loadMore: function () {
+    var self = this;
+    // 当前页是最后一页
+    if (self.data.currentPage == self.data.allPages) {
+      self.setData({
+        loadMoreData: '已经到顶'
+      })
+      return;
+    }
+    setTimeout(function () {
+      console.log('上拉加载更多');
+      var tempCurrentPage = self.data.currentPage;
+      tempCurrentPage = tempCurrentPage + 1;
+      self.setData({
+        currentPage: tempCurrentPage,
+        hideBottom: false
+      })
+      self.getData();
+    }, 300);
+  },
+  // 下拉刷新
+  refresh: function (e) {
+    var self = this;
+    setTimeout(function () {
+      console.log('下拉刷新');
+      var date = new Date();
+      self.setData({
+        currentPage: 1,
+        refreshTime: date.toLocaleTimeString(),
+        hideHeader: false
+      })
+      self.getData();
+    }, 300);
+  },
+  // 获取数据  pageIndex：页码参数
+  getData: function () {
+    var self = this;
+    var pageIndex = self.data.currentPage;
+    wx.request({
+      url: 'https://route.showapi.com/582-2',
+      data: {
+        showapi_appid: '19297',
+        showapi_sign: 'cf606a68a01f45d196b0061a1046b5b3',
+        page: pageIndex
+      },
+      success: function (res) {
+        var dataModel = res.data;
+        if (dataModel.showapi_res_code == 0) {
+          if (dataModel.showapi_res_body.ret_code == 0) {
+            if (pageIndex == 1) { // 下拉刷新
+              self.setData({
+                allPages: dataModel.showapi_res_body.pagebean.allPages,
+                contentlist: dataModel.showapi_res_body.pagebean.contentlist,
+                hideHeader: true
+              })
+            } else { // 加载更多
+              console.log('加载更多');
+              var tempArray = self.data.contentlist;
+              tempArray = tempArray.concat(dataModel.showapi_res_body.pagebean.contentlist);
+              self.setData({
+                allPages: dataModel.showapi_res_body.pagebean.allPages,
+                contentlist: tempArray,
+                hideBottom: true
+              })
+            }
+          }
+        }
+      },
+      fail: function () {
+
+      }
+    })
   }
 
 
